1a384463a739e3a1c0a06d9c988b39780fc4b875
dimitrijjedich <dimitrij@startup-werk.de>
2025-06-19 12:19:49 +0200

    update createUser script

--- creatUser.sh
+++ creatUser.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker exec -u root -it reverse-proxy htpasswd -c /etc/nginx/.htpasswd $1
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: <username>"
+  exit 1
+fi
+
+if docker exec reverse-proxy test -f /etc/nginx/.htpasswd; then
+  docker exec -u root -it reverse-proxy htpasswd /etc/nginx/.htpasswd "$1"
+else
+  docker exec -u root -it reverse-proxy htpasswd -c /etc/nginx/.htpasswd "$1"
+fi
+
+echo "User '$1' added to reverse proxy"
